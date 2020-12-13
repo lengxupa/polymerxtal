e5f4cc322880879cc68ed19f3a6fd0e7ba4a135e
Tongtong Shen <shen276@purdue.edu>
2020-12-13 18:11:06 -0500

    copy necessary polymerxtal to /simtool directory

--- simtool/polymerxtal/crystal/defect.py
+++
@@ -1,4 +0,0 @@
-"""
-This module is for functions which handle defects in polymer crystals.
-"""
-
--- simtool/polymerxtal/data/__init__.py
+++ simtool/polymerxtal/data/__init__.py
@@ -1,2 +1,1 @@
 from .atom_data import atomic_weights, atom_colors, atomic_radii
-from .sample import sample_chain
